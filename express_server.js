50d00e1f5153e58c084c84fdda9cd66f8e4bbb19
Maxim Nechaev <4max.nechaev@gmail.com>
2018-08-07 19:17:42 -0400

    Template for the future app is created

---
+++ express_server.js
@@ -0,0 +1,29 @@
+/*jshint esversion: 6 */
+const http = require("http");
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+  res.render("urls_index");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+app.listen(PORT);
+console.log(`Server is listening to port ${PORT}`);
