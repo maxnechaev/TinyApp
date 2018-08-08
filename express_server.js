6f553b475b7838d7ca49777b2da0525ff02d5fef
Maxim Nechaev <4max.nechaev@gmail.com>
2018-08-08 15:35:16 +0000

    Delete function added

--- express_server.js
+++ express_server.js
@@ -3,16 +3,46 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
+const bodyParser = require("body-parser");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "CfGU4Q": "http://www.facebook.com"
 };
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  var shortURL = "";
+  var abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 6; i++){
+    shortURL += abc.charAt(Math.floor(Math.random() * abc.length));
+  }
+  return shortURL;
+}
+
+console.log(generateRandomString());
 
 app.get("/", (req, res) => {
-  res.render("urls_index");
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  res.redirect(urlDatabase[shortURL]);
 });
 
 app.get("/urls", (req, res) => {
@@ -20,10 +50,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
-  res.render("urls_show", templateVars);
+app.post("/urls", (req, res) => {
+  var shortURL = generateRandomString();
+  var longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  console.log(urlDatabase);
+  res.send(`<html><body><a href="http://localhost:8080/u/${shortURL}">http://localhost:8080/u/${shortURL}</a></body></html>`);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/");
 });
 
 app.listen(PORT);
 console.log(`Server is listening to port ${PORT}`);
+generateRandomString();
